ee78105b4c126ce9a97b078ac18cb08e604e7780
Luke Turner <contact@luketurner.org>
2013-04-19 11:22:31 -0700

    Reduce depth for shorter profiling times

--- othello/othello/player.py
+++ othello/othello/player.py
@@ -173,7 +173,7 @@ class CPUPlayer:
 		root = BoardNode(self.board, [], self.us)
 
 		def prune(child):
-			a = self.ab_prune(child, 8, NegativeInfinity(), Infinity())
+			a = self.ab_prune(child, 6, NegativeInfinity(), Infinity())
 			print(child.move, a)
 			return a
 
